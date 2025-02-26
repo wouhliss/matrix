2db4d482babc4db3088850c1325c98f6eddd0bf2
Walid Ouhlisse <143995405+wouhliss@users.noreply.github.com>
2025-02-26 15:16:08 +0100

    to fix: generic types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use num_traits::{MulAdd, MulAddAssign, One};
 use std::fmt::{Display, Formatter, Result};
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
@@ -11,14 +12,19 @@ pub trait Field:
     + Mul<Output = Self>
     + MulAssign
     + Mul
+    + MulAdd<Output = Self>
+    + MulAddAssign
+    + MulAdd
     + Div<Output = Self>
     + DivAssign
     + Div
     + Neg<Output = Self>
-    + Into<f64>
     + PartialEq
+    + PartialOrd
     + Copy
     + Default
+    + One
+    + Into<f32>
 {
 }
 
@@ -32,14 +38,19 @@ impl<
             + Mul<Output = Self>
             + MulAssign
             + Mul
+            + MulAdd<Output = Self>
+            + MulAddAssign
+            + MulAdd
             + Div<Output = Self>
             + DivAssign
             + Div
             + Neg<Output = Self>
-            + Into<f64>
             + PartialEq
+            + PartialOrd
             + Copy
-            + Default,
+            + Default
+            + One
+            + Into<f32>,
     > Field for T
 {
 }
@@ -101,16 +112,74 @@ impl<K: Field, const N: usize> Vector<K, N> {
         }
     }
 
+    pub fn dot(&self, v: Self) -> K {
+        let mut res = K::default();
+
+        for (x, num) in self.data.iter().enumerate() {
+            res = num.mul_add(v.data[x], res);
+        }
+
+        res
+    }
+
+    pub fn norm_1(self) -> K {
+        let mut res = K::default();
+
+        for x in self.data.iter() {
+            res += if *x >= -*x { *x } else { -*x };
+        }
+
+        res
+    }
+
+    pub fn norm(self) -> f32 {
+        let mut res = K::default();
+
+        for x in self.data.iter() {
+            res = x.mul_add(*x, res);
+        }
+
+        res.into().powf(0.5)
+    }
+
+    pub fn norm_inf(self) -> K {
+        let mut res = if self.data[0] >= -self.data[0] {
+            self.data[0]
+        } else {
+            -self.data[0]
+        };
+
+        for x in self.data.iter() {
+            res = if *x >= -*x { *x } else { -*x }
+        }
+
+        res
+    }
+
     pub fn linear_combination(u: &[Vector<K, N>], coefs: &[K]) -> Vector<K, N> {
         let mut res: [K; N] = [K::default(); N];
 
         for (x, vec) in u.iter().enumerate() {
             for (y, num) in vec.data.iter().enumerate() {
-                res[y] += *num * coefs[x];
+                res[y] = num.mul_add(coefs[x], res[y]);
             }
         }
 
         Vector::from(res)
+    }
+
+    pub fn angle_cos(u: &Vector<K, N>, v: &Vector<K, N>) -> f32 {
+        u.dot(*v).into() / (u.norm() * v.norm())
+    }
+}
+
+impl<K: Field> Vector<K, 3> {
+    pub fn cross_product(u: &Vector<K, 3>, v: &Vector<K, 3>) -> Vector<K, 3> {
+        Vector::<K, 3>::new(&[
+            u.data[1] * v.data[2] - u.data[2] * v.data[1],
+            u.data[2] * v.data[0] - u.data[0] * v.data[2],
+            u.data[0] * v.data[1] - u.data[1] * v.data[0],
+        ])
     }
 }
 
@@ -121,7 +190,7 @@ impl<K: Field, const N: usize> From<[K; N]> for Vector<K, N> {
 }
 
 pub fn lerp<V: Field>(u: V, v: V, t: V) -> V {
-    u + (v - u) * t
+    (v - u).mul_add(t, u)
 }
 
 impl<K: Field, const N: usize> Vector<K, N> {
@@ -129,20 +198,20 @@ impl<K: Field, const N: usize> Vector<K, N> {
         let mut res = [K::default(); N];
 
         for idx in 0..u.data.len() {
-            res[idx] = u.data[idx] + (v.data[idx] - u.data[idx]) * t;
+            res[idx] = (v.data[idx] - u.data[idx]).mul_add(t, u.data[idx]);
         }
 
         Vector::from(res)
     }
 }
 
-impl<K: Field, const N: usize> Matrix<K, N> {
-    pub fn lerp(u: Matrix<K, N>, v: Matrix<K, N>, t: K) -> Matrix<K, N> {
-        let mut res = [[K::default(); N]; N];
+impl<K: Field, const N: usize, const M: usize> Matrix<K, N, M> {
+    pub fn lerp(u: Matrix<K, N, M>, v: Matrix<K, N, M>, t: K) -> Matrix<K, N, M> {
+        let mut res = [[K::default(); N]; M];
 
         for x in 0..u.data.len() {
             for y in 0..u.data[x].len() {
-                res[x][y] = u.data[x][y] + (v.data[x][y] - u.data[x][y]) * t;
+                res[x][y] = (v.data[x][y] - u.data[x][y]).mul_add(t, u.data[x][y]);
             }
         }
 
@@ -151,11 +220,11 @@ impl<K: Field, const N: usize> Matrix<K, N> {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Matrix<K, const N: usize> {
-    data: [[K; N]; N],
+pub struct Matrix<K, const N: usize, const M: usize> {
+    data: [[K; N]; M],
 }
 
-impl<K: Field + Display, const N: usize> Display for Matrix<K, N> {
+impl<K: Field + Display, const N: usize, const M: usize> Display for Matrix<K, N, M> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         for row in self.data.iter() {
             write!(f, "[")?;
@@ -171,12 +240,12 @@ impl<K: Field + Display, const N: usize> Display for Matrix<K, N> {
     }
 }
 
-impl<K: Field, const N: usize> Matrix<K, N> {
-    pub fn new(data: [[K; N]; N]) -> Self {
-        Matrix { data }
+impl<K: Field, const N: usize, const M: usize> Matrix<K, N, M> {
+    pub fn new(data: &[[K; N]; M]) -> Self {
+        Matrix { data: data.clone() }
     }
 
-    pub fn add(&mut self, v: Matrix<K, N>) {
+    pub fn add(&mut self, v: Matrix<K, N, M>) {
         for i in 0..self.data.len() {
             if let Some(r1) = self.data.get_mut(i) {
                 if let Some(r2) = v.data.get(i) {
@@ -192,7 +261,7 @@ impl<K: Field, const N: usize> Matrix<K, N> {
         }
     }
 
-    pub fn sub(&mut self, v: Matrix<K, N>) {
+    pub fn sub(&mut self, v: Matrix<K, N, M>) {
         for i in 0..self.data.len() {
             if let Some(r1) = self.data.get_mut(i) {
                 if let Some(r2) = v.data.get(i) {
@@ -215,10 +284,36 @@ impl<K: Field, const N: usize> Matrix<K, N> {
             }
         }
     }
+
+    pub fn mul_vec(self, v: &Vector<K, N>) -> Vector<K, M> {
+        let mut vec = Vector::<K, M>::new(&[K::default(); M]);
+
+        for y in 0..M {
+            for x in 0..N {
+                vec.data[y] = self.data[y][x].mul_add(v.data[x], vec.data[y]);
+            }
+        }
+
+        vec
+    }
+
+    pub fn mul_mat(self, m: &Matrix<K, M, N>) -> Matrix<K, N, M> {
+        let mut mat = Matrix::<K, N, M>::new(&[[K::default(); N]; M]);
+
+        for y in 0..M {
+            for x in 0..N {
+                for z in 0..N {
+                    mat.data[y][x] = self.data[y][z].mul_add(m.data[z][x], mat.data[y][x]);
+                }
+            }
+        }
+
+        mat
+    }
 }
 
-impl<K: Field, const N: usize> From<[[K; N]; N]> for Matrix<K, N> {
-    fn from(array: [[K; N]; N]) -> Self {
-        Matrix::new(array)
+impl<K: Field, const N: usize, const M: usize> From<[[K; N]; M]> for Matrix<K, N, M> {
+    fn from(array: [[K; N]; M]) -> Self {
+        Matrix::new(&array)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,45 @@
 use matrix::*;
 
 fn main() {
-    let mut u = Vector::from([2., 3.]);
+    let u = Vector::from([2., 3.]);
     let v = Vector::from([5., 7.]);
     u.add(v);
     println!("{}", u);
+    // [7.0]
+    // [10.0]
 
-    let mut u = Vector::from([2., 3.]);
+    let u = Vector::from([2., 3.]);
     let v = Vector::from([5., 7.]);
     u.sub(v);
     println!("{}", u);
+    // [-3.0]
+    // [-4.0]
 
     let mut u = Vector::from([2., 3.]);
     u.scl(2.);
     println!("{}", u);
+    // [4.0]
+    // [6.0]
 
     let mut u = Matrix::from([[1., 2.], [3., 4.]]);
     let v = Matrix::from([[7., 4.], [-2., 2.]]);
     u.add(v);
     println!("{}", u);
+    // [8.0, 6.0]
+    // [1.0, 6.0]
 
     let mut u = Matrix::from([[1., 2.], [3., 4.]]);
     let v = Matrix::from([[7., 4.], [-2., 2.]]);
     u.sub(v);
     println!("{}", u);
+    // [-6.0, -2.0]
+    // [5.0, 2.0]
 
     let mut u = Matrix::from([[1., 2.], [3., 4.]]);
     u.scl(2.);
     println!("{}", u);
+    // [2.0, 4.0]
+    // [6.0, 8.0]
 
     let e1 = Vector::from([1., 0., 0.]);
     let e2 = Vector::from([0., 1., 0.]);
@@ -39,21 +51,33 @@ fn main() {
         "{}",
         Vector::linear_combination(&[e1, e2, e3], &[10., -2., 0.5])
     );
+    // [10.]
+    // [-2.]
+    // [0.5]
 
     println!("{}", Vector::linear_combination(&[v1, v2], &[10., -2.]));
+    // [10.]
+    // [0.]
+    // [230.]
 
     println!("{}", lerp(0., 1., 0.));
+    // 0.0
 
     println!("{}", lerp(0., 1., 1.));
+    // 1.0
 
     println!("{}", lerp(0., 1., 0.5));
+    // 0.5
 
     println!("{}", lerp(21., 42., 0.3));
+    // 27.3
 
     println!(
         "{}",
         Vector::lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3)
     );
+    // [2.6]
+    // [1.3]
 
     println!(
         "{}",
@@ -63,4 +87,108 @@ fn main() {
             0.5
         )
     );
+    // [[11., 5.5]
+    // [16.5, 22.]]
+
+    let u = Vector::from([0., 0.]);
+    let v = Vector::from([1., 1.]);
+    println!("{}", u.dot(v));
+    // 0.0
+    let u = Vector::from([1., 1.]);
+    let v = Vector::from([1., 1.]);
+    println!("{}", u.dot(v));
+    // 2.0
+    let u = Vector::from([-1., 6.]);
+    let v = Vector::from([3., 2.]);
+    println!("{}", u.dot(v));
+    // 9.0
+
+    let u = Vector::from([0., 0., 0.]);
+    println!("{}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // 0.0, 0.0, 0.0
+    let u = Vector::from([1., 2., 3.]);
+    println!("{}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // 6.0, 3.74165738, 3.0
+    let u = Vector::from([-1., -2.]);
+    println!("{}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // 3.0, 2.236067977, 2.0
+
+    let u = Vector::from([1., 0.]);
+    let v = Vector::from([1., 0.]);
+    println!("{}", Vector::angle_cos(&u, &v));
+    // 1.0
+    let u = Vector::from([1., 0.]);
+    let v = Vector::from([0., 1.]);
+    println!("{}", Vector::angle_cos(&u, &v));
+    // 0.0
+    let u = Vector::from([-1., 1.]);
+    let v = Vector::from([1., -1.]);
+    println!("{}", Vector::angle_cos(&u, &v));
+    // -1.0
+    let u = Vector::from([2., 1.]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", Vector::angle_cos(&u, &v));
+    // 1.0
+    let u = Vector::from([1., 2., 3.]);
+    let v = Vector::from([4., 5., 6.]);
+    println!("{}", Vector::angle_cos(&u, &v));
+    // 0.974631846
+
+    let u = Vector::from([0., 0., 1.]);
+    let v = Vector::from([1., 0., 0.]);
+    println!("{}", Vector::cross_product(&u, &v));
+    // [0.]
+    // [1.]
+    // [0.]
+    let u = Vector::from([1., 2., 3.]);
+    let v = Vector::from([4., 5., 6.]);
+    println!("{}", Vector::cross_product(&u, &v));
+    // [-3.]
+    // [6.]
+    // [-3.]
+    let u = Vector::from([4., 2., -3.]);
+    let v = Vector::from([-2., -5., 16.]);
+    println!("{}", Vector::cross_product(&u, &v));
+    // [17.]
+    // [-58.]
+    // [-16.]
+
+    let u = Matrix::from([[1., 0.], [0., 1.]]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", u.mul_vec(&v));
+    // [4.]
+    // [2.]
+    let u = Matrix::from([[2., 0.], [0., 2.]]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", u.mul_vec(&v));
+    // [8.]
+    // [4.]
+    let u = Matrix::from([[2., -2.], [-2., 2.]]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", u.mul_vec(&v));
+    // [4.]
+    // [-4.]
+    let u = Matrix::from([[2., -2.], [-2., 2.], [2., -2.]]);
+    let v = Vector::from([4., 2.]);
+    println!("{}", u.mul_vec(&v));
+    // [4.]
+    // [-4.]
+    // [4.]
+
+    let u = Matrix::from([[1., 0.], [0., 1.]]);
+    let v = Matrix::from([[1., 0.], [0., 1.]]);
+    println!("{}", u.mul_mat(&v));
+    // [1., 0.]
+    // [0., 1.]
+    let u = Matrix::from([[1., 0.], [0., 1.]]);
+    let v = Matrix::from([[2., 1.], [4., 2.]]);
+    println!("{}", u.mul_mat(&v));
+    // [2., 1.]
+    // [4., 2.]
+    let u = Matrix::from([[3., -5.], [6., 8.], [6., 8.]]);
+    let v = Matrix::from([[2., 1., 1.], [4., 2., 1.]]);
+    println!("{}", u.mul_mat(&v));
+    // [-14., -7.]
+    // [44., 22.]
+    // [44., 22.]
 }
